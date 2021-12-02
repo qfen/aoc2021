fca081f71b67de577d08d46b62da1d3a0d41e529
Dan F (freebie VM) <dan@proinbox.com>
2021-12-02 10:25:20 -0500

    add day 1 part 2

--- day01/day01.pl
+++ day01/day01.pl
@@ -6,17 +6,26 @@
 use v5.28;
 use warnings;
 
-my($prev, $count) = (~0, 0);
-my($int, $line);
+my $int;
+my $prev = ~0; # for part 1
+my @windows; # for part 2
+my($part1, $part2) = (0, 0);
 
-while ($line = <STDIN>) {
+while (my $line = <STDIN>) {
 	chomp $line;
-	warn "non-numeric input" if $line =~ /[^\d]/;
-
+	warn "non-numeric" if $line =~ /[^\d]/;
 	$int = int $line;
-	$count++ if $int > $prev;
+
+	# part 1: count number of times this input is larger than previous line
+	$part1++ if $int > $prev;
 	$prev = $int;
+
+	# part 2: count number of times this 3-line sliding window is greater
+	# than the preceding window
+	$windows[$_] += $int for (1 .. 3);
+	$part2++ if $. > 3 and $windows[1] > $windows[0];
+	shift @windows;
 }
 
-# output bare number of times that output increased over previous line
-say $count;
+say "part 1: $part1";
+say "part 2: $part2";
