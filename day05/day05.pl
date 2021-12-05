5fb6720555846aa364326d165cfacb98bd097dfc
Dan F (freebie VM) <dan@proinbox.com>
2021-12-05 08:50:43 -0500

    day 5 part 2

--- day05/day05.pl
+++ day05/day05.pl
@@ -5,27 +5,34 @@
 
 use v5.28;
 use warnings;
-use List::Util 'reduce';
+use List::Util qw(max reduce);
 
-my @coords;
-my @map;
+my (@map_part1, @map_part2, $line);
 my ($x1, $y1, $x2, $y2);
-my $line;
+my $diag; # boolean for part 2
+my ($xdir, $ydir, $xdist, $ydist, $x, $y);
+
+sub sign { $_[0] < 0 ? -1 : ($_[0] == 0 ? 0 : 1); }
 
 while ($line = <STDIN>) {
 	chomp $line;
 	$line =~ /^(\d+),(\d+) -> (\d+),(\d+)$/ or warn 'unusual line';
 	($x1, $y1, $x2, $y2) = map { int } ($1, $2, $3, $4);
-	next unless ($x1 == $x2 or $y1 == $y2); # diagonal line
 
-	($x1, $x2) = ($x2, $x1) if $x1 > $x2;
-	($y1, $y2) = ($y2, $y1) if $y1 > $y2;
+	$xdist = abs($x2 - $x1);
+	$ydist = abs($y2 - $y1);
+	$xdir = sign($x2 - $x1);
+	$ydir = sign($y2 - $y1);
+	$diag = $xdir && $ydir;
+	die "not snapped to 45 degrees" if $diag and !($xdist == $ydist);
 
-	for my $x ($x1 .. $x2) { # looks like n^2 but is actually O(n)
-		for my $y ($y1 .. $y2) {
-			$map[$x][$y]++;
-		}
+	for my $i (0 .. max($xdist, $ydist)) {
+		$x = $x1 + $i * $xdir;
+		$y = $y1 + $i * $ydir;
+		$map_part2[$x][$y]++;
+		$map_part1[$x][$y]++ unless $diag;
 	}
 }
 
-say reduce { $a + scalar(grep {($_ // 0) > 1} @$b) } (0, @map);
+say 'part 1: ', reduce { $a + grep {($_ // 0) > 1} @$b } 0, @map_part1;
+say 'part 2: ', reduce { $a + grep {($_ // 0) > 1} @$b } 0, @map_part2;
