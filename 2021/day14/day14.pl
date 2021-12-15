6d37bdf7d459d7e1ee07020e919c71abd5ade231
Dan F (wsl-alpine) <dan@proinbox.com>
2021-12-14 19:46:36 -0500

    day 14 part 2, structure still a little iffy

--- 2021/day14/day14.pl
+++ 2021/day14/day14.pl
@@ -3,28 +3,37 @@ use v5.28;
 use warnings;
 use List::Util qw(max min);
 
-my ($line, %rule, @poly, @work, $i, $n);
-my (%part1);
+sub score { max(values $_[0]->%*) - min(values $_[0]->%*) }
 
+my ($line, $prev);
+my (%rule, %count, %work, %temp, $num, $ref);
+
+# reading input
 $line = <STDIN>;
 chomp $line;
-@poly = split //, $line;
+for (split //, $line) {
+	$count{$_}++;
+	$work{$prev . $_}++ if $prev;
+	$prev = $_;
+}
 
 while ($line = <STDIN>) {
 	next unless $line =~ /^([A-Z]{2}) -> ([A-Z])$/;
-	$rule{$1} = $2;
+	$rule{$1} = [ $2, substr($1, 0, 1) . $2, $2 . substr($1, 1, 1) ];
 }
 
-$n = 10;
-@work = @poly;
-while ($n--) {
-	say $n;
-	for ($i = $#poly; $i; $i--) {
-		splice @work, $i, 0, $rule{$poly[$i - 1] . $poly[$i]};
+# doing work
+for (my $n = 1; $n <= 40; $n++) {
+	%temp = ();
+	for my $input (keys %work) {
+		$num = $work{$input};
+		$ref = $rule{$input};
+		$count{$ref->[0]} += $num;
+		$temp{$ref->[1]} += $num;
+		$temp{$ref->[2]} += $num;
 	}
-	@poly = @work;
+	%work = %temp;
+	say 'part 1: ', score(\%count) if $n == 10;
 }
 
-$part1{$_}++ for @poly;
-
-say 'part 1: ', max(values %part1) - min(values %part1);
+say 'part 2: ', score(\%count);
