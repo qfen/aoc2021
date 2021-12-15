615ee0895aa5544515a29e538442bd6f57d987ff
Dan F (wsl-alpine) <dan@proinbox.com>
2021-12-14 20:04:27 -0500

    refactor into a subroutine; slightly cleaner

--- 2021/day14/day14.pl
+++ 2021/day14/day14.pl
@@ -3,37 +3,40 @@ use v5.28;
 use warnings;
 use List::Util qw(max min);
 
+my (%rule, %count, $prev);
+my $work = {};
+
 sub score { max(values $_[0]->%*) - min(values $_[0]->%*) }
 
-my ($line, $prev);
-my (%rule, %count, %work, %temp, $num, $ref);
+sub polymer { # pass hashref of pairs to be expanded once
+	my (%temp, $num, $ref);
 
-# reading input
-$line = <STDIN>;
-chomp $line;
-for (split //, $line) {
-	$count{$_}++;
-	$work{$prev . $_}++ if $prev;
-	$prev = $_;
-}
-
-while ($line = <STDIN>) {
-	next unless $line =~ /^([A-Z]{2}) -> ([A-Z])$/;
-	$rule{$1} = [ $2, substr($1, 0, 1) . $2, $2 . substr($1, 1, 1) ];
-}
-
-# doing work
-for (my $n = 1; $n <= 40; $n++) {
-	%temp = ();
-	for my $input (keys %work) {
-		$num = $work{$input};
+	for my $input (keys $_[0]->%*) {
+		$num = $_[0]->{$input};
 		$ref = $rule{$input};
 		$count{$ref->[0]} += $num;
 		$temp{$ref->[1]} += $num;
 		$temp{$ref->[2]} += $num;
 	}
-	%work = %temp;
-	say 'part 1: ', score(\%count) if $n == 10;
+
+	return \%temp; # new state of worklist
 }
 
+while (<STDIN>) {
+	chomp;
+	if (/^[A-Z]+$/) { # "polymer template"
+		for (split //, $_) {
+			$count{$_}++;
+			$work->{$prev . $_}++ if $prev;
+			$prev = $_;
+		}
+	} elsif (/^([A-Z]{2}) -> ([A-Z])$/) { # "pair insertion" rules
+		$rule{$1} = [ $2, substr($1, 0, 1) . $2, $2 . substr($1, 1, 1) ];
+	}
+}
+
+$work = polymer($work) for (1 .. 10);
+say 'part 1: ', score(\%count);
+
+$work = polymer($work) for (11 .. 40);
 say 'part 2: ', score(\%count);
