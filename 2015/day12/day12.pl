dbfb5049de2ac956cc2c361fddf939a0d2683365
Dan F (wsl-alpine) <dan@proinbox.com>
2022-05-27 17:04:00 -0400

    2015 day 12 part 2 (perl)

--- 2015/day12/day12.pl
+++ 2015/day12/day12.pl
@@ -4,30 +4,42 @@ use warnings;
 use Scalar::Util qw(looks_like_number reftype);
 use JSON::PP;
 
-my $sum = 0;
 sub recurse {
 	my $ref = shift;
+	my $part1 = 0;
+	my $part2 = 0;
+	my $has_red = 0;
 	for (reftype $ref) {
 		no warnings 'experimental::smartmatch';
 		when (!defined $_) {
-			$sum += $ref if looks_like_number $ref;
+			$part1 = $part2 = $ref if looks_like_number $ref;
 		}
 
 		when ('ARRAY') {
-			recurse($_) for (@$ref);
+			for (@$ref) {
+				my ($temp1, $temp2) = recurse($_);
+				$part1 += $temp1;
+				$part2 += $temp2;
+			}
 		}
 
 		when ('HASH') {
-			recurse($ref->{$_}) for (keys %$ref);
+			for (keys %$ref) {
+				my ($temp1, $temp2) = recurse($ref->{$_});
+				$part1 += $temp1;
+				$part2 += $temp2;
+				$has_red = 1 if 'red' eq lc $ref->{$_};
+			}
 		}
 	}
+
+	return ($part1, $has_red ? 0 : $part2);
 }
 
 my $jref;
+
 {	local $/;
-	my $json = <STDIN>;
-	$jref = decode_json $json;
+	$jref = decode_json <STDIN>;
 }
 
-recurse($jref);
-say 'part 1: ', $sum;
+printf "part 1: %d\npart 2: %d\n", recurse($jref);
