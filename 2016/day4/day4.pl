2dc604078f2e3d9c8e1de8dcdddd2b1cca48fa7d
Dan F (WSL alpine) <dan@proinbox.com>
2022-11-09 13:33:57 -0500

    2016 day 4 part 2 (perl)

--- 2016/day4/day4.pl
+++ 2016/day4/day4.pl
@@ -3,19 +3,30 @@ use v5.18;
 use warnings;
 use List::Util 'reduce';
 
-my $part1;
+my ($part1, $part2);
 
 while (<>) {
 	/^([-a-z]+)(\d+)\[([a-z]+)\]$/ or die 'weird line';
 
-	my $letters = reduce { $a->{$b}++, $a }
-		+{}, grep { $_ ne '-' } split //, $1;
+	my $sector = int $2;
+	my (%freq, $decode);
+
+	for (split //, $1) {
+		if ($_ eq '-') {
+			$decode .= ' ';
+		} else {
+			$freq{$_}++;
+			$decode .= chr( ((-97 + ord $_) + $sector) % 26 + 97 );
+		}
+	}
 
 	my @counts = sort { $b->[0] <=> $a->[0] || $a->[1] cmp $b->[1] }
-		map { [ $letters->{$_}, $_ ] }
-		keys %$letters;
+		map { [ $freq{$_}, $_ ] }
+		keys %freq;
 
-	$part1 += $2 * ($3 eq join '', map { $_->[1] } @counts[0 .. 4]);
+	$part1 += $sector * ($3 eq join '', map { $_->[1] } @counts[0 .. 4]);
+	$part2 = $sector if $decode =~ /northpole/;
 }
 
 say "part 1: $part1";
+say "part 2: $part2";
