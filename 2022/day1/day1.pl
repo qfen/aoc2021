4b95e1f682aa88bf2d097c8d1eb9f192a955176c
Dan (macbook) <dan@proinbox.com>
2022-12-01 08:30:33 -0500

    minor variants for handling list

--- 2022/day1/day1.pl
+++ 2022/day1/day1.pl
@@ -1,11 +1,18 @@
 #!/usr/bin/perl
 use v5.18;
 use warnings;
-use List::Util 'sum';
+use List::Util qw(reduce sum);
 
 $/ = '';
 
-my @elves = sort { $b <=> $a } map { sum map { int } split /\s+/ } <>;
+# eliminate temporary list
+#my @elves = sort { $b <=> $a } map { reduce { $a + $b } split /\s+/ } <>;
+
+# explicit numeric conversion
+#my @elves = sort { $b <=> $a } map { sum map { int } split /\s+/ } <>;
+
+# concise
+my @elves = sort { $b <=> $a } map { sum split /\s+/ } <>;
 
 say "part 1: ", $elves[0];
 say "part 2: ", sum @elves[0 .. 2];
