dfd3eff6bb3fcc1bb75b5d47c4bf2c00e2935828
Dan F (WSL alpine) <dan@proinbox.com>
2022-12-09 09:10:19 -0500

    2022 day 9 part 2 (perl)
    removed a layer of indirection; not really worth the effort

--- 2022/day9/day9.pl
+++ 2022/day9/day9.pl
@@ -2,27 +2,31 @@
 use v5.18;
 use warnings;
 
-my %part1;
-my ($hx, $hy, $tx, $ty) = (0, 0, 0, 0);
 my %dirs = (L => [-1, 0], R => [ 1, 0], U => [ 0, 1], D => [ 0,-1]);
-my ($diffx, $diffy);
+my @knots = (0) x 20;
+my (%part1, %part2);
 
 while (<>) {
 	/^([LRUD]) (\d+)$/ or die 'weird line';
 
 	for (1 .. $2) {
-		$hx += $dirs{$1}->[0];
-		$hy += $dirs{$1}->[1];
-		$diffx = $hx - $tx;
-		$diffy = $hy - $ty;
-		if (abs($diffx) == 2 or abs($diffy) == 2) {
-			$tx += ($diffx <=> 0);
-			$ty += ($diffy <=> 0);
+		$knots[0] += $dirs{$1}->[0];
+		$knots[1] += $dirs{$1}->[1];
+
+		for (my $i = 2; $i < 20; $i += 2) {
+			my $diffx = $knots[$i - 2] - $knots[$i];
+			my $diffy = $knots[$i - 1] - $knots[$i + 1];
+
+			if (abs($diffx) == 2 or abs($diffy) == 2) {
+				$knots[  $i  ] += ($diffx <=> 0);
+				$knots[$i + 1] += ($diffy <=> 0);
+			}
 		}
 
-		$part1{"$tx,$ty"} = undef;
-		#say "$1 h($hx,$hy) t($tx,$ty)";
+		$part1{"@knots[2,3]"} = undef;
+		$part2{"@knots[18,19]"} = undef;
 	}
 }
 
 say 'part 1: ', scalar keys %part1;
+say 'part 2: ', scalar keys %part2;
