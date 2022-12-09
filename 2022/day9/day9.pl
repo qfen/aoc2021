bc5408542d34bc4229bebca18efa2b3cb433f663
Dan F (WSL alpine) <dan@proinbox.com>
2022-12-09 09:15:25 -0500

    2022 day 9 part 2 (perl)

--- 2022/day9/day9.pl
+++ 2022/day9/day9.pl
@@ -3,28 +3,28 @@ use v5.18;
 use warnings;
 
 my %dirs = (L => [-1, 0], R => [ 1, 0], U => [ 0, 1], D => [ 0,-1]);
-my @knots = (0) x 20;
+my @knots = map { [0, 0] } 1 .. 10;
 my (%part1, %part2);
 
 while (<>) {
 	/^([LRUD]) (\d+)$/ or die 'weird line';
 
 	for (1 .. $2) {
-		$knots[0] += $dirs{$1}->[0];
-		$knots[1] += $dirs{$1}->[1];
+		$knots[0]->[0] += $dirs{$1}->[0];
+		$knots[0]->[1] += $dirs{$1}->[1];
 
-		for (my $i = 2; $i < 20; $i += 2) {
-			my $diffx = $knots[$i - 2] - $knots[$i];
-			my $diffy = $knots[$i - 1] - $knots[$i + 1];
+		for my $i (1 .. $#knots) {
+			my $diffx = $knots[$i - 1]->[0] - $knots[$i]->[0];
+			my $diffy = $knots[$i - 1]->[1] - $knots[$i]->[1];
 
 			if (abs($diffx) == 2 or abs($diffy) == 2) {
-				$knots[  $i  ] += ($diffx <=> 0);
-				$knots[$i + 1] += ($diffy <=> 0);
+				$knots[$i]->[0] += ($diffx <=> 0);
+				$knots[$i]->[1] += ($diffy <=> 0);
 			}
 		}
 
-		$part1{"@knots[2,3]"} = undef;
-		$part2{"@knots[18,19]"} = undef;
+		$part1{ "@{ $knots[1] }" } = undef;
+		$part2{ "@{ $knots[9] }" } = undef;
 	}
 }
 
