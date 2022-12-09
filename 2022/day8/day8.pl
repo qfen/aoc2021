31fbdab5ed5806fb6a5efe26d0eaeb9155dfc181
Dan F (WSL alpine) <dan@proinbox.com>
2022-12-08 20:58:32 -0500

    less repetitive but slower

--- 2022/day8/day8.pl
+++ 2022/day8/day8.pl
@@ -3,7 +3,7 @@ use v5.18;
 use warnings;
 use List::Util 'max';
 
-# (height, visible, scenic) tuples
+my @moves = ([0, -1], [0, 1], [-1, 0], [1, 0]);
 my $trees = [ map { chomp; [ map { int } split // ] } <> ];
 
 my $x_end = $#{ $trees->[0] };
@@ -13,59 +13,35 @@ my $part2 = 0;
 
 for my $row (1 .. $y_end - 1) {
 	for my $col (1 .. $x_end - 1) {
-		my $height = $trees->[$row][$col];
 		my ($scene, $vis) = (1, 0);
-		my ($max, $dist);
 
-		# moving left
-		($max, $dist) = (0, undef);
-		for my $x (reverse 0 .. $col - 1) {
-			if (!$dist and $trees->[$row][$x] >= $height) {
-				$dist = $col - $x;
-			}
-			$max = max $max, $trees->[$row][$x];
-		}
-		$scene *= $dist // $col;
-		$vis = 1 if $height > $max;
+		for my $dir (@moves) {
+			my ($x, $y, $max) = ($col, $row, 0);
+			my ($dist, $step);
 
-		# moving right
-		($max, $dist) = (0, undef);
-		for my $x ($col + 1 .. $x_end) {
-			if (!$dist and $trees->[$row][$x] >= $height) {
-				$dist = $x - $col;
-			}
-			$max = max $max, $trees->[$row][$x];
-		}
-		$scene *= $dist // ($x_end - $col);
-		$vis = 1 if $height > $max;
+			while () {
+				$x += $dir->[1];
+				$y += $dir->[0];
+				$step++;
 
-		# moving up
-		($max, $dist) = (0, undef);
-		for my $y (reverse 0 .. $row - 1) {
-			if (!$dist and $trees->[$y][$col] >= $height) {
-				$dist = $row - $y;
-			}
-			$max = max $max, $trees->[$y][$col];
-		}
-		$scene *= $dist // $row;
-		$vis = 1 if $height > $max;
+				last unless 0 <= $x <= $x_end and 0 <= $y <= $y_end;
 
-		# moving down
-		($max, $dist) = (0, undef);
-		for my $y ($row + 1 .. $y_end) {
-			if (!$dist and $trees->[$y][$col] >= $height) {
-				$dist = $y - $row;
+				if (!$dist and $trees->[$y][$x] >= $trees->[$row][$col]) {
+					$dist = $step;
+				}
+
+				$max = max $max, $trees->[$y][$x];
 			}
-			$max = max $max, $trees->[$y][$col];
+
+			$vis = 1 if $trees->[$row][$col] > $max;
+			$scene *= $dist // $step - 1;
 		}
-		$scene *= $dist // ($y_end - $row);
-		$vis = 1 if $height > $max;
 
 		$part1 += $vis;
 		$part2 = max $scene, $part2;
 	}
 }
 
-#say join(' ', map { "(@$_)" } @$_) for @$trees;
+#say join(' ',  @$_) for @$trees;
 say "part 1: $part1";
 say "part 2: $part2";
